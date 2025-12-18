812a9437cddab5ce2e34eabbe3b24f68703cd28d
samio11 <samiohasan6@gmail.com>
2025-12-18 18:16:22 +0600

    middlewares for formData validateRequest added

--- dist/app.js
+++ dist/app.js
@@ -4,11 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors_1 = __importDefault(require("cors"));
+const GlobalErrorHandler_1 = require("./app/middlewares/GlobalErrorHandler");
+const NotFound_1 = require("./app/middlewares/NotFound");
 const app = (0, express_1.default)();
-app.use(express_1.default.json());
 app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use((0, cookie_parser_1.default)());
 app.get("/", (req, res) => {
     res.status(200).json({ message: "Server is running successfully🚕" });
 });
+app.use(GlobalErrorHandler_1.GlobalErrorHandler);
+app.use(NotFound_1.NotFound);
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express, { Application, Request, Response } from "express";
+import cookieParser from "cookie-parser";
 import cors from "cors";
+import { GlobalErrorHandler } from "./app/middlewares/GlobalErrorHandler";
+import { NotFound } from "./app/middlewares/NotFound";
 const app: Application = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Server is running successfully🚕" });
 });
+
+app.use(GlobalErrorHandler);
+app.use(NotFound);
 
 export default app;
